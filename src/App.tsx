7676328030ae3ac2c791ba7b086264b7dc4cc052
c-hillyard <hillyard.colleen@gmail.com>
2023-06-09 07:29:12 -0400

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import ProjectsPage from './projects/ProjectsPage';
 import Hello from './Hello';
 
-function App() {
-  // return (
-  //   <blockquote cite='Benjamin Franklin'>
-  //     Tell me and I forget, teach me and I may remember, involve me and I learn.
-  //   </blockquote>
-  // );
+const App = () => {
   return (
     <div className='container'>
       <blockquote cite='Benjamin Franklin'>
