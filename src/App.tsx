719cdb73a0fec20f110b6c2dd819c794956c69bd
c-hillyard <hillyard.colleen@gmail.com>
2023-06-09 07:34:35 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import './App.css';
-import ProjectsPage from './projects/ProjectsPage';
-import Hello from './Hello';
 
 const App = () => {
   return (
     <div className='container'>
-      <blockquote cite='Benjamin Franklin'>
-        Tell me and I forget, teach me and I may remember, involve me and I learn.
-      </blockquote>
-      <ProjectsPage />
-      <Hello name='David' />
+      Tailwind
     </div>
   )
 }
