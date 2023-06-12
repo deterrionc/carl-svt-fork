8bb6471689d2d4d4b7ea28802f553127b3d2d09b
c-hillyard <hillyard.colleen@gmail.com>
2023-06-12 08:07:56 -0400

    feat: video show page

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,24 +1,10 @@
 import Header from "../../container/organisms/Header"
 import ArticleList from "../../container/organisms/ArticleList"
 import VideoList from "../../container/organisms/VideoList"
+import tempData from '../../utils/temp.json'
 
-const topArticles = [
-  { id: '1', image: "/assets/img/card-list-1.jpg", tag: "svt news", title: "So the battles can be affected by the dust collapse", description: "This is War of the Week - delving into the lastest in the war in Ukraine" },
-  { id: '2', image: "/assets/img/card-list-2.jpg", tag: "new episode", title: "Wild chefs", description: "Six profiessional chefs compete to cook the best food over an open fire" },
-  { id: '3', image: "/assets/img/card-list-3.jpg", tag: "lotta edholm", title: "30 minutes", description: "Anders Holmberg's guest is education minister Lotta Edholm" },
-  { id: '4', image: "/assets/img/card-list-4.jpg", tag: "new drama comedy", title: "From the area", description: "Two words collide when Zeina is forced to move from the suburbs of Stockholm to a farm in Varmland" },
-  { id: '5', image: "/assets/img/card-list-5.jpg", tag: "News Directly", title: "Unique prosecution of terroist financing", description: "A man in his 40s is charged today with, among other things, aggravated extortion and attempted financing of terrorism" },
-  { id: '6', image: "/assets/img/card-list-6.jpg", tag: "lastest news", title: "The morning studio", description: "Today's most important news and analysis with frequent updates" },
-]
-
-const topVideos = [
-  { id: '1', image: "/assets/img/video-list-1.jpg", title: "Farewell and onion planting" },
-  { id: '2', image: "/assets/img/video-list-2.jpg", title: "Season 1 - Episode 1" },
-  { id: '3', image: "/assets/img/video-list-3.jpg", title: "Trailer: Premier Leagure: Battle Giant" },
-  { id: '4', image: "/assets/img/video-list-4.jpg", title: "The first Swedes" },
-  { id: '5', image: "/assets/img/video-list-5.jpg", title: "30 minuter Lotta Edholm" },
-  { id: '6', image: "/assets/img/video-list-6.jpg", title: "Julia Kalthoff Hemmakultur - Konst" },
-]
+const topArticles = tempData.articles
+const topVideos = tempData.videos
 
 const Home = () => {
   return (
--- src/components/home/videos/video-room/index.js
+++ src/components/home/videos/video-room/index.js
@@ -2,7 +2,8 @@ import { Link } from "react-router-dom"
 import { FaIcon } from "../../../../container/atoms/FaIcon"
 import Header from "../../../../container/organisms/Header"
 
-const VideoRoom = () => {
+const VideoRoom = ({ match }) => {
+  console.log(match)
   return (
     <div className="min-h-screen bg-svt">
       <div className="container mx-auto text-white">
@@ -11,6 +12,10 @@ const VideoRoom = () => {
           <Link to='/videos'>
             <h5 className="m-1 pb-3 text-lg font-bold leading-6">Videos <FaIcon iconName="fa-arrow-right" /></h5>
           </Link>
+
+          <video className="min-w-full" autoPlay controls>
+            <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4" />
+          </video>
         </div>
       </div>
     </div>
